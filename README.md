9c6809c41455d41bc9a7cea50e7bead3853b750e
DoubleDa <dayonxin@gmail.com>
2016-07-19 00:19:10 +0800

    Commit Eg

--- README.md
+++ README.md
@@ -4,3 +4,24 @@ java提高篇学习记录
 ## java提高篇之数组（1）：认识JAVA数组
 
 [学习地址](http://www.importnew.com/20666.html)
+
+* 什么是数组
+
+	数组是一个简单的复合数据类型，它是一系列有序数据的集合，它当中的每一个数据都具有相同的数据类型，我们通过数组名加上一个不会越界下标值来唯一确定数组中的元素。
+	
+[有关JVM处理Java数组方法的思考](http://developer.51cto.com/art/201001/176671.htm)
+
+		数组array类名：[I
+		数组array父类：class java.lang.Object
+		数组array2类名：[[I
+		数组array3类名：[[[I
+		***************
+		Object[]:class [Ljava.lang.Object;
+		Object[][]:class [[Ljava.lang.Object;
+		Object[][][]:class [[[Ljava.lang.Object;
+
+	
+普通的java类是以**全限定路径名+类名**来作为自己的唯一标示的，而数组则是以**若干个[+L+数组元素类全限定路径+类**来最为唯一标示的。
+
+
+
--- src/com/dyx/sjp/chapter1/JavaArray.java
+++ src/com/dyx/sjp/chapter1/JavaArray.java
@@ -16,5 +16,28 @@ public class JavaArray {
 		int[] array = new int[10];
 		System.out.println("数组array类名：" + array.getClass().getName());
 		System.out.println("数组array父类：" + array.getClass().getSuperclass());
+
+		int[][] array2 = new int[10][10];
+		System.out.println("数组array2类名：" + array2.getClass().getName());
+
+		int[][][] array3 = new int[10][10][10];
+		System.out.println("数组array3类名：" + array3.getClass().getName());
+
+		System.out.println("***************");
+
+		// 数组的类名＝'['+数组元素类型的内部名称
+		System.out.println("Object[]:" + Object[].class);
+		System.out.println("Object[][]:" + Object[][].class);
+		System.out.println("Object[][][]:" + Object[][][].class);
+
+		System.out.println("***************");
+		
+		int[] myArray = new int[10];
+		Class cla = myArray.getClass();
+		System.out.println(cla.getDeclaredFields().length);
+		System.out.println(cla.getDeclaredMethods().length);
+		System.out.println(cla.getDeclaredConstructors().length);
+		System.out.println(cla.getDeclaredAnnotations().length);
+		System.out.println(cla.getDeclaredClasses().length);
 	}
 }
